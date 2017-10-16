3b55de2101bcfa8d28da3c10a3655d2cc2af087b
Sal <sgavin@uci.edu>
2017-10-16 00:00:27 -0700

    Added a pause menu with working resume and log out button

---
+++ Assets/Scripts/StopGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Stop()
+    {
+        //Stops game if it is being run in the unity editor
+        if (UnityEditor.EditorApplication.isPlaying)
+            UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
