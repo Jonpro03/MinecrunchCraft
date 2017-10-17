5416e407729b9ff2069be3a9067dec60cebef69c
JonProietti <jon.proietti@watchfiresigns.com>
2017-10-16 22:04:59 -0500

    Added threading, not sure that it helped...

--- Assets/Scripts/StopGame.cs
+++ Assets/Scripts/StopGame.cs
@@ -16,10 +16,11 @@ public class StopGame : MonoBehaviour {
 
     public void Stop()
     {
+#if UNITY_EDITOR
         //Stops game if it is being run in the unity editor
         if (UnityEditor.EditorApplication.isPlaying)
             UnityEditor.EditorApplication.isPlaying = false;
-
+#endif
         //Need to add functionality for game when actually running that will properly exit/save
     }
 }
