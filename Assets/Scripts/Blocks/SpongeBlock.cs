f308c5554a3b998443beb2c177549832bb14845d
Sal <sgavin@uci.edu>
2017-10-16 18:05:23 -0700

    Added scripts for blocks gold ore, iron ore, coal ore, oak/spurce wood log, birch wood, jungle wood, sponge, wet sponge, glass, lapis lazuili ore block, lapis lazuli block, sandstone and chiseled sandstone

---
+++ Assets/Scripts/Blocks/SpongeBlock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.Blocks
+{
+    public class SpongeBlock : Block
+    {
+        public override string Texture { get { return "Materials/SpongeBlock"; } }
+
+        public override bool IsTransparent { get { return false; } }
+
+        public override bool IsGravityAffected { get { return false; } }
+
+        public override uint MiningDifficulty { get { return MiningDifficulties.NO_TOOL; } }
+
+        public override string SoundWalkedOnAsset { get { return "Sounds/SpongeWalk"; } }
+
+        public override string SoundBeingMinedAsset { get { return "Sounds/SpongeMined"; } }
+
+        public override string SoundBlockPlacedAsset { get { return "Sounds/SpongePlaced"; } }
+
+        public string Color { get; set; }
+
+        public SpongeBlock(Vector3 chunkPos, Vector2 chunkLoc) : base(chunkPos, chunkLoc)
+        {
+
+        }
+
+        public override void OnDestroyed()
+        {
+
+        }
+
+        public override void OnPlaced()
+        {
+
+        }
+    }
+}
